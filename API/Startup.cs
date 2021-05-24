f16dfc40c255595baa9d88989cdf884cd30a2eaa
Stanislau <tovtivil@gmail.com>
2021-05-24 03:11:11 +0300

    Walking skeleton of react part is done

--- API/Startup.cs
+++ API/Startup.cs
@@ -20,13 +20,13 @@ namespace API
 {
   public class Startup
   {
-      private readonly IConfiguration _config;
+    private readonly IConfiguration _config;
 
-      public Startup(IConfiguration config)
-      {
-          _config = config;
-          //Configuration = configuration;
-      }
+    public Startup(IConfiguration config)
+    {
+      _config = config;
+      //Configuration = configuration;
+    }
 
     //public IConfiguration Configuration { get; }
 
@@ -41,8 +41,15 @@ namespace API
       });
       services.AddDbContext<DataContext>(opt =>
       {
-          opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+        opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
       });
+      services.AddCors(opt =>
+      opt.AddPolicy("CorsPolicy", policy => policy
+      .AllowAnyMethod()
+      .AllowAnyHeader()
+      .WithOrigins("http://localhost:3000"))
+      //.WithOrigins("http://localhost:3000/"))
+      );
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +65,7 @@ namespace API
       // app.UseHttpsRedirection();
 
       app.UseRouting();
+      app.UseCors("CorsPolicy");
 
       app.UseAuthorization();
 
